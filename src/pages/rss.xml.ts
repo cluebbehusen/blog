8f8195f368e888e536d7dcbbbaf581645c20006f
Connor Luebbehusen <connorluebbehusen@gmail.com>
2025-12-27 16:36:15 -0500

    rss tweaks and add image

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,24 +1,36 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt({ html: true });
 
 export async function GET(context: { site: URL }) {
   const posts = await getCollection('blog');
+  const siteUrl = context.site.toString().replace(/\/$/, '');
+
   return rss({
     title: 'luebbehusen.dev',
     description: 'A blog about software and other things',
     site: context.site,
-    customData: `<image>
+    customData: `<atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml"/>
+    <image>
       <url>${context.site}icon.png</url>
       <title>luebbehusen.dev</title>
       <link>${context.site}</link>
     </image>`,
+    xmlns: { atom: 'http://www.w3.org/2005/Atom' },
     items: posts
       .sort((a, b) => b.data.published.valueOf() - a.data.published.valueOf())
-      .map((post) => ({
-        title: post.data.title,
-        pubDate: post.data.published,
-        description: post.data.description,
-        link: `/blog/${post.slug}/`,
-      })),
+      .map((post) => {
+        const html = parser.render(post.body ?? '');
+        const content = html.replace(/src="\/([^"]+)"/g, `src="${siteUrl}/$1"`);
+        return {
+          title: post.data.title,
+          pubDate: post.data.published,
+          description: post.data.description,
+          link: `/blog/${post.slug}/`,
+          content,
+        };
+      }),
   });
 }
